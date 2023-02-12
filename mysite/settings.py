89cf5d98e7957bb22aec7ca04f6858f90d286011
leords <leo_ro.drigues@hotmail.com>
2023-02-12 20:27:29 -0300

    new config for allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#4&74!^@2sc2mrg88gt)ay#f=l-(ye2*3*k16*vx60xg3jksya
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
